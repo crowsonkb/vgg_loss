94db7ef7318efe07035ef08d756c50a1b0ec35f0
Katherine Crowson <crowsonkb@gmail.com>
2021-01-11 15:29:40 -0800

    Rename variables

--- vgg_loss_demo.py
+++ vgg_loss_demo.py
@@ -11,12 +11,12 @@ import vgg_loss
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    crit = vgg_loss.VGGLoss().to(device)
+    crit_vgg = vgg_loss.VGGLoss().to(device)
     crit_tv = vgg_loss.TVLoss(p=2)
 
     target = tio.read_image('DSC00261.jpg')[None] / 255
     target = TF.resize(target, (256, 256), 3).to(device)
-    target_act = crit.get_features(target)
+    target_act = crit_vgg.get_features(target)
 
     input = torch.rand_like(target) / 255 + 0.5
     input.requires_grad_(True)
@@ -26,7 +26,7 @@ def main():
     try:
         for i in range(1000):
             opt.zero_grad()
-            loss = crit(input, target_act, target_is_features=True)
+            loss = crit_vgg(input, target_act, target_is_features=True)
             loss += crit_tv(input) * 20
             print(i, loss.item())
             loss.backward()
