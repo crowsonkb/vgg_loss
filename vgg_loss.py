80765369d1d0f972ed341e28c3d9e399a287ba71
Katherine Crowson <crowsonkb@gmail.com>
2021-01-19 23:30:16 -0800

    Implement vectorial total variation

--- vgg_loss.py
+++ vgg_loss.py
@@ -46,10 +46,15 @@ class TVLoss(nn.Module):
     The input must be 4D. If a target (second parameter) is passed in, it is
     ignored.
 
-    ``p=1`` yields the originally proposed (isotropic) 2D total variation
-    norm (see https://en.wikipedia.org/wiki/Total_variation_denoising).
+    ``p=1`` yields the vectorial total variation norm. It is a generalization
+    of the originally proposed (isotropic) 2D total variation norm (see
+    (see https://en.wikipedia.org/wiki/Total_variation_denoising) for color
+    images. On images with a single channel it is equal to the 2D TV norm.
+
     ``p=2`` yields a variant that is often used for smoothing out noise in
-    reconstructions of images from neural network feature maps.
+    reconstructions of images from neural network feature maps (see Mahendran
+    and Vevaldi, "Understanding Deep Image Representations by Inverting
+    Them", https://arxiv.org/abs/1412.0035)
 
     :attr:`reduction` can be set to ``'mean'``, ``'sum'``, or ``'none'``
     similarly to the loss functions in :mod:`torch.nn`. The default is
@@ -58,6 +63,10 @@ class TVLoss(nn.Module):
 
     def __init__(self, p, reduction='mean', eps=1e-8):
         super().__init__()
+        if p not in {1, 2}:
+            raise ValueError('p must be 1 or 2')
+        if reduction not in {'mean', 'sum', 'none'}:
+            raise ValueError("reduction must be 'mean', 'sum', or 'none'")
         self.p = p
         self.reduction = reduction
         self.eps = eps
@@ -66,7 +75,9 @@ class TVLoss(nn.Module):
         input = F.pad(input, (0, 1, 0, 1), 'replicate')
         x_diff = input[..., :-1, :-1] - input[..., :-1, 1:]
         y_diff = input[..., :-1, :-1] - input[..., 1:, :-1]
-        diff = (x_diff**2 + y_diff**2 + self.eps)**(self.p / 2)
+        diff = x_diff**2 + y_diff**2
+        if self.p == 1:
+            diff = (diff + self.eps).mean(dim=1, keepdims=True).sqrt()
         if self.reduction == 'mean':
             return diff.mean()
         if self.reduction == 'sum':
